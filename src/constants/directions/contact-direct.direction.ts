8a6ac9da06dd6015f771ab6fc93e0a88caf1471c
ThanhTai1309 <tai73559@gmail.com>
2024-12-17 21:25:51 +0700

    fix deploy v2

--- src/constants/directions/contact-direct.direction.ts
+++ src/constants/directions/contact-direct.direction.ts
@@ -1,2 +1,3 @@
 export const MAIL_TO = 'mailto:thanhtung2888@gmail.com?subject=Inquiry&body=Hello%20Thanh%20Tung,%0D%0A%0D%0AI%20have%20a%20question%20regarding...'
 export const PHONE_TO = 'tel:+84909090909'
+export const FACEBOOK_TO = 'https://www.facebook.com/thanhtung2888'
