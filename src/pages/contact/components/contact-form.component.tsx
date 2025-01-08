ef0f10683c214bbcd811908f04a344f331a15537
ThanhTai1309 <tai73559@gmail.com>
2025-01-08 21:42:07 +0700

    custom button

--- src/pages/contact/components/contact-form.component.tsx
+++ src/pages/contact/components/contact-form.component.tsx
@@ -9,14 +9,15 @@ const ContactForm = () => {
             <input type="text" id="name" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter Your Full Name" required />
           </div>
           <div>
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">Your Email</label>
             <input type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Enter Your Email" required />
           </div>
           <div className="sm:col-span-2">
-            <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your message</label>
+            <label htmlFor="message" className="block mb-2 text-sm font-medium text-white">Your Message</label>
             <textarea id="message" rows={6} className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
           </div>
-          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          <button type="submit" className="py-3 px-5 text-sm font-medium text-center text-white bg-gradient-to-r from-green-400 to-blue-500 hidden md:inline transform transition-transform 
+              duration-300 hover:scale-105 px-4 py-2 rounded-full">Send message</button>
         </form>
       </section>
     </>
